57e3463711dba222a77e9afb86612f5be6c0c860
deeppaz <deeppaz@hotmail.com>
2020-07-02 21:08:24 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Header from './Header/Header';
 import Searching from './Search/Searching';
 import Body from './Body/Body';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Searching />
-      <Body tanim="tanım gelecek"/>
-    </div>
-  );
+import filterLugat from './filterLugat';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Searching />
+        <Body tanim="tanım gelecek" />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
